c96b6a0f8c7e4f260a9427a00a7dc32a4c092472
LuccMitZweiC <berluc20@htl-kaindorf.at>
2023-05-02 09:52:31 +0200

    Sign Up und Log In page (gestohlen)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import {useEffect, useState} from "react";
 import {createClient} from "@supabase/supabase-js";
 
-const supabase = createClient("http://localhost:8000/", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICAgInJvbGUiOiAiYW5vbiIsCiAgICAiaXNzIjogInN1cGFiYXNlIiwKICAgICJpYXQiOiAxNjgyMjg3MjAwLAogICAgImV4cCI6IDE4NDAxNDAwMDAKfQ.srs0zQzoqjCJzpjtYmII5Jx35UaR8nZOqSVfV92YB7M");
+const supabase = createClient("http://192.168.238.48:8000/", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICAgInJvbGUiOiAiYW5vbiIsCiAgICAiaXNzIjogInN1cGFiYXNlIiwKICAgICJpYXQiOiAxNjgyMjg3MjAwLAogICAgImV4cCI6IDE4NDAxNDAwMDAKfQ.srs0zQzoqjCJzpjtYmII5Jx35UaR8nZOqSVfV92YB7M");
 
 function App() {
     const [countries, setCountries] = useState([]);
