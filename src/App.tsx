0402abb4fa7753db9ceed4c0dc04699fbf1ba13b
LuccMitZweiC <berluc20@htl-kaindorf.at>
2023-05-16 11:53:16 +0200

    tailwind und die scheiß ip

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import CountryList from "./components/Views/Countries.tsx";
 import Login from "./components/Views/Login.tsx";
 import LoginPage from "./components/Views/Login.tsx";
 
-const supabase : SupabaseClient = createClient("http://localhost:8000/", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICAgInJvbGUiOiAiYW5vbiIsCiAgICAiaXNzIjogInN1cGFiYXNlIiwKICAgICJpYXQiOiAxNjgyMjg3MjAwLAogICAgImV4cCI6IDE4NDAxNDAwMDAKfQ.srs0zQzoqjCJzpjtYmII5Jx35UaR8nZOqSVfV92YB7M");
+const supabase : SupabaseClient = createClient("http://192.168.216.48:8000/", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICAgInJvbGUiOiAiYW5vbiIsCiAgICAiaXNzIjogInN1cGFiYXNlIiwKICAgICJpYXQiOiAxNjgyMjg3MjAwLAogICAgImV4cCI6IDE4NDAxNDAwMDAKfQ.srs0zQzoqjCJzpjtYmII5Jx35UaR8nZOqSVfV92YB7M");
 
 function App() {
 
